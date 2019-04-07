1b1aafb78a65c5ab1c9d154372791864b276f633
Rohit <rohitjha941@hotmail.com>
2019-04-07 09:16:51 +0530

    added static url

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path,include, re_path
 from django.conf.urls import url
 from django.views.generic import  TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
