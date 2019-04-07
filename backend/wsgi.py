4ff2259c0add9a0466e6c15e9e3f63b321752d90
Rohit <rohitjha941@hotmail.com>
2019-04-07 09:02:26 +0530

    removed whitenoise settings

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,8 +3,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+ 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+ 
