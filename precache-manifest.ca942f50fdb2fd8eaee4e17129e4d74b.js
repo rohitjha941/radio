d9590321c2ee8ea2b329bd2346414c23e57d4dc3
Rohit <rohitjha941@hotmail.com>
2019-04-07 09:31:00 +0530

    Updates

---
+++ precache-manifest.ca942f50fdb2fd8eaee4e17129e4d74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36f3067e7a782fbb952b01f35cc63297",
+    "url": "/static/media/background.36f3067e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "767efad22d4e566f7e71",
+    "url": "/static/js/main.6affc612.chunk.js"
+  },
+  {
+    "revision": "938e6c389329eab56bc0",
+    "url": "/static/js/2.341fb1c6.chunk.js"
+  },
+  {
+    "revision": "767efad22d4e566f7e71",
+    "url": "/static/css/main.b2ece942.chunk.css"
+  },
+  {
+    "revision": "25841e543ec11c124664733ae381da20",
+    "url": "/index.html"
+  }
+];
