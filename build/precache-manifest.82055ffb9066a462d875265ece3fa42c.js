93016d71452875ff9ce63919605946028544bf5e
Rohit <rohitjha941@hotmail.com>
2019-04-07 08:22:49 +0530

    Changes for herouku deployment

---
+++ build/precache-manifest.82055ffb9066a462d875265ece3fa42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36f3067e7a782fbb952b01f35cc63297",
+    "url": "/static/media/background.36f3067e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66e6034c1b995db2ee11",
+    "url": "/static/js/main.315138ee.chunk.js"
+  },
+  {
+    "revision": "938e6c389329eab56bc0",
+    "url": "/static/js/2.341fb1c6.chunk.js"
+  },
+  {
+    "revision": "66e6034c1b995db2ee11",
+    "url": "/static/css/main.b2ece942.chunk.css"
+  },
+  {
+    "revision": "a64a4eb598e5dd51e1b37acd563e1843",
+    "url": "/index.html"
+  }
+];
